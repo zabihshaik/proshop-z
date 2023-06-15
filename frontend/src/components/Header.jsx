1d3ef22fd5349703737549972a06915ae2181792
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-15 15:18:39 +0530

    feat: custom bootstrap & logo added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant = "dark" expand = "md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">ProShop-Z</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt='ProShop-Z' />
+                    ProShop-Z
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 

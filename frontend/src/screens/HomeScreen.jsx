3b7652368fcae2cc625a26c67f20dc0fbfbdb9f9
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-17 21:45:35 +0530

    feat: products list as Cards on Home page done

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
 import products from '../products';
 
 const HomeScreen = () => {
@@ -8,8 +9,8 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         { products.map((product) =>(
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product}/>
           </Col>
         ))}
       </Row>

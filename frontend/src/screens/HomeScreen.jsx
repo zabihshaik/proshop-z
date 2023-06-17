4d873585ed4ddf392e703bdc1ff050cfc4ae2604
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-17 21:22:41 +0530

    feat: product names on home screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import products from '../products';
 
 const HomeScreen = () => {
   return (
-    <div>HomeScreen</div>
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        { products.map((product) =>(
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <h3>{product.name}</h3>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
-export default HomeScreen
+export default HomeScreen;

035b30b54ad131cd24ea96c21cc2756bc04b183b
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-21 15:04:08 +0530

    feat: fetched products from backend using axios

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
-import products from '../products';
 import Rating from '../components/Rating';
+import axios from 'axios';
 
 
 const ProductScreen = () => {
+    const [ product, setProduct] = useState({});
+
     const  { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId);
-
-
+    
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    }, [productId]);
    
   return (
     <>

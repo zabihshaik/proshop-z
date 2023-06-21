09045e98fc50a786e50fca7b14c785b48d23c00a
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-22 00:27:27 +0530

    feat: Connected with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 const port = process.env.PORT;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 

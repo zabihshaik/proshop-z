ac4b396068e12b2f87b873a4eb9b36c5a6dfcdc5
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-25 00:14:33 +0530

    feat: (fetching data from DB) productRoutes done

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json(products);
+});
+
+router.get('/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT;
 
@@ -14,13 +15,6 @@ app.get('/', (req, res) => {
     res.send('API is running....');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

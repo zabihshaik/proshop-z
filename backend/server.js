5848f952ed815e990b8b53f7da63c60b24a56d50
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-26 22:11:22 +0530

    feat: fetching products from DB, asyncHandler done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
 import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT;

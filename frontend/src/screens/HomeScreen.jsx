5a71fe6a08dd9c63a46842b72d5c3f78f4df8da1
Zabihulla Shaik <informshaikz@gmail.com>
2023-06-29 20:54:11 +0530

    feat: productsAPISlice & getProducts endpoint done

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() =>{
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
+      { isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (<div>{ error?.data?.message || error.error }</div>) : (<>
       <h1>Latest Products</h1>
       <Row>
-        { products.map((product) =>(
+        {products.map((product) =>(
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product}/>
           </Col>
         ))}
-      </Row>
+      </Row></>) }
+
+      
     </>
   )
 }
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -6,5 +6,5 @@ const baseQuery = fetchBaseQuery({baseUrl: BASE_URL});
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ['Product', 'Order', 'User'],
-    endpoints: (builder) => ({})
+    endpoints: (builder) => ({}),
 });

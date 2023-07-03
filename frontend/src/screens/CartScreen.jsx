24138631f233451deabdb201ecb1868fe9494cd1
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-03 23:32:06 +0530

    feat: Cart Screen part done

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import Message from '../components/Message';
+
+const CartScreen = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    
+  return (
+    <div>CartScreen</div>
+  )
+}
+
+export default CartScreen;

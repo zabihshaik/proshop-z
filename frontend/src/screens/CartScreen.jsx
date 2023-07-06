d86d6ca4bca768179ee2bc555b73f9a4beea9096
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-06 22:31:36 +0530

    feat: Remove from cart done

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
     const navigate = useNavigate();
@@ -15,6 +15,10 @@ const CartScreen = () => {
 
     const addToCartHandler = async (product, qty) => {
         dispatch(addToCart({...product, qty}));
+    }
+
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id));
     }
 
   return (
@@ -52,7 +56,7 @@ const CartScreen = () => {
                                     </Form.Control>
                                 </Col>
                                 <Col md={2}>
-                                    <Button type='button' variant='light'>
+                                    <Button type='button' variant='light' onClick={() => removeFromCartHandler(item._id)}>
                                         <FaTrash />
                                     </Button>
                                 </Col>

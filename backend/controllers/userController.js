1df72b9df1de34b5c05e9dfd7a6865dd4fd6d513
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-08 22:54:30 +0530

    feat: backend - user email & password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,26 @@
-import asyncHandler from "../middleware/asyncHandler.js";
-import User from "../models/userModel.js";
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 
 const authUser = asyncHandler(async (req, res) => {
-    res.send('auth user');
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc    Register User
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -23,6 +24,10 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model('User', userSchema);
 
 export default User;

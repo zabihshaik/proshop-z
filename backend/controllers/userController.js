235879ea279f50e3005def84f7268b46c5dfe667
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-09 23:37:50 +0530

    fix: jwt http-only cookie maxAge fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,7 @@ const authUser = asyncHandler(async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV !== 'development',
             sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 // 30 Days
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
         });
 
         res.json({

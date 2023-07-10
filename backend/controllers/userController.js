d976dd0a868f8e6ec0494f30242e0ea1af41b025
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-10 23:29:53 +0530

    feat: backend auth- logout user, clear cookie done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,7 +49,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access  Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // @desc    Get User Profile

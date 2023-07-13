87f4e10cf813bd693bfaa1936c6632a2bed5c63f
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-13 23:58:55 +0530

    feat: frontendAuth-Login Screen done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,13 +11,15 @@ import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} /> 
-      <Route path='/cart' element={<CartScreen />} /> 
+      <Route path='/cart' element={<CartScreen />} />      
+      <Route path='/login' element={<LoginScreen />} />      
     </Route>
   )
 )

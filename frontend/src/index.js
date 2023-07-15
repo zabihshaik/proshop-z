96a30412ca6049ff0c190d24c427d5e0eb08613f
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-15 21:44:14 +0530

    feat:frontendAuth-user registration done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,8 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,6 +22,7 @@ const router = createBrowserRouter(
       <Route path='/product/:id' element={<ProductScreen />} /> 
       <Route path='/cart' element={<CartScreen />} />      
       <Route path='/login' element={<LoginScreen />} />      
+      <Route path='/register' element={<RegisterScreen />} />      
     </Route>
   )
 )
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -4,18 +4,20 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import Loader from '../components/Loader';
-import { useLoginMutation } from '../slices/usersApiSlice';
+import { useRegisterMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast } from 'react-toastify';
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const [login, { isLoading }] = useLoginMutation();
+    const [register, { isLoading }] = useRegisterMutation();
 
     const { userInfo } = useSelector((state) => state.auth);
 
@@ -31,20 +33,35 @@ const LoginScreen = () => {
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        try {
-           const res = await login({ email, password }).unwrap();
-           dispatch(setCredentials({ ...res, }));
-           navigate(redirect); 
-        } catch (err) {
-            toast.error(err?.data?.message || err.error);
+
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match');
+            return;
+        } else {
+            try {
+                const res = await register({ name, email, password }).unwrap();
+                dispatch(setCredentials({ ...res, }));
+                navigate(redirect); 
+             } catch (err) {
+                 toast.error(err?.data?.message || err.error);
+             }
         }
     }
 
   return (
     <FormContainer>
-        <h1>Sign In</h1>
+        <h1>Sign Up</h1>
 
         <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name' className='my-3'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}></Form.Control>
+            </Form.Group>
+
             <Form.Group controlId='email' className='my-3'>
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control
@@ -63,8 +80,17 @@ const LoginScreen = () => {
                 onChange={(e) => setPassword(e.target.value)}></Form.Control>
             </Form.Group>
 
+            <Form.Group controlId='confirmPassword' className='my-3'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                type='password'
+                placeholder='Confirm password'
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+            </Form.Group>
+
             <Button type='submit' variant='primary' className='mt-2' disabled= { isLoading }>
-                Sign In
+                Register
             </Button>
 
             { isLoading && <Loader /> }
@@ -72,11 +98,11 @@ const LoginScreen = () => {
 
         <Row className='py-3'>
             <Col>
-                New Customer?{' '}<Link to={ redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                Already have an Account?{' '}<Link to={ redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
             </Col>
         </Row>
     </FormContainer>
   )
 }
 
-export default LoginScreen;
+export default RegisterScreen;

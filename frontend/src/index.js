00b0ba0958ce1d97b3199f27d41990c6d16fc577
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-18 23:49:37 +0530

    feat: checkout-payment method done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 
 const router = createBrowserRouter(
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />} /> 
+        <Route path='/payment' element={<PaymentScreen />} /> 
       </Route>
 
     </Route>

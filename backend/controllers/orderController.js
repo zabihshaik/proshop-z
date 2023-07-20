a67600664e520625496c1ff7b9e630665f820efb
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-20 23:04:33 +0530

    feat: checkout_backend-create & get orders done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,39 @@ import Order from "../models/orderModel.js";
 // @access  Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-    res.send('add order items');
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items');
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined,
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
 });
 
 // @desc    Get logged in user orders
@@ -14,7 +46,8 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-    res.send('get my orders');
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
@@ -22,7 +55,14 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getOrderbyId = asyncHandler(async (req, res) => {
-    res.send('get order by id');
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 // @desc    Update order to paid

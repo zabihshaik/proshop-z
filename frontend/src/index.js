70101dfe9c63a13ac85eb609f6fcc9e3ab7f1ba3
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-05 23:48:53 +0530

    feat: admin-list products for admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,6 +22,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import OrderListScreen from './screens/admin/OrderListScreen';
+import ProductListScreen from './screens/admin/ProductListScreen';
 
 
 const router = createBrowserRouter(
@@ -43,6 +44,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
+        <Route path='/admin/productlist' element={<ProductListScreen />} />
       </Route>
 
     </Route>

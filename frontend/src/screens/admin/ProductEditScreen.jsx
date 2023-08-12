6e47c2af504a3803dd4b24782e290608d67c9a62
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-13 00:53:42 +0530

    Edit Product by Admin

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -18,7 +18,7 @@ const ProductEditScreen = () => {
     const [countInStock, setCountInStock] = useState(0);
     const [description, setDescription] = useState('');
 
-    const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId);
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
     const [updateProduct, { isLoading: loadingUpdate }] = useUpdateProductMutation();
 
@@ -35,7 +35,29 @@ const ProductEditScreen = () => {
             setDescription(product.description);
         }
     }, [product]);
-  
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const updatedProduct = {
+            productId,
+            name,
+            price,
+            image,
+            brand,
+            category,
+            countInStock,
+            description, 
+        };
+
+        const result = await updateProduct(updatedProduct);
+        if (result.error) {
+         toast.error(result.error);
+        } else {
+         toast.success('Product updated');
+         navigate('/admin/productlist');
+        }
+    }
+
     return (
         <>
             <Link to='/admin/productList' className='btn btn-light my-3'> 
@@ -47,16 +69,72 @@ const ProductEditScreen = () => {
 
                 {isLoading ? <Loader /> : error ? <Message variant='danger'>
                     {error}</Message> : (
-                        <Form>
-                            <Form.Group controlId='name'>
+                        <Form onSubmit={submitHandler}>
+                            <Form.Group controlId='name' className='my-2'>
                                 <Form.Label>Name</Form.Label>
                                 <Form.Control 
-                                   type='name'
+                                   type='text'
                                    placeholder='Enter name of product'
                                    value={name}
                                    onChange={(e) => setName(e.target.value)}
                                 ></Form.Control>        
                             </Form.Group>
+
+                            <Form.Group controlId='price' className='my-2'>
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control 
+                                   type='number'
+                                   placeholder='Enter price'
+                                   value={price}
+                                   onChange={(e) => setPrice(e.target.value)}
+                                ></Form.Control>        
+                            </Form.Group>
+
+                            {/* IMAGE INPUT PLACEHOLDER */}
+
+                            <Form.Group controlId='brand' className='my-2'>
+                                <Form.Label>Brand</Form.Label>
+                                <Form.Control 
+                                   type='text'
+                                   placeholder='Enter brand'
+                                   value={brand}
+                                   onChange={(e) => setBrand(e.target.value)}
+                                ></Form.Control>        
+                            </Form.Group>
+
+                            <Form.Group controlId='countInStock' className='my-2'>
+                                <Form.Label>Count In Stock</Form.Label>
+                                <Form.Control 
+                                   type='number'
+                                   placeholder='Enter count in stock'
+                                   value={countInStock}
+                                   onChange={(e) => setCountInStock(e.target.value)}
+                                ></Form.Control>        
+                            </Form.Group>
+
+                            <Form.Group controlId='category' className='my-2'>
+                                <Form.Label>Category</Form.Label>
+                                <Form.Control 
+                                   type='text'
+                                   placeholder='Enter category'
+                                   value={category}
+                                   onChange={(e) => setCategory(e.target.value)}
+                                ></Form.Control>        
+                            </Form.Group>
+
+                            <Form.Group controlId='description' className='my-2'>
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control 
+                                   type='text'
+                                   placeholder='Enter description'
+                                   value={description}
+                                   onChange={(e) => setDescription(e.target.value)}
+                                ></Form.Control>        
+                            </Form.Group>
+
+                            <Button type='submit' variant='primary' className='my-2'>
+                                Update
+                            </Button>
                         </Form>
                     )}
             </FormContainer>
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,6 +7,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             query: () => ({
                 url: PRODUCTS_URL,
             }),
+            providesTags: ['Products'],
             keepUnusedDataFor: 5,
         }),
         getProductDetails: builder.query({
@@ -24,11 +25,11 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         }),
         updateProduct: builder.mutation({
             query: (data) => ({
-                url: `${PRODUCTS_URL}/${data._id}`,
+                url: `${PRODUCTS_URL}/${data.productId}`,
                 method: 'PUT',
                 body: data,
             }),
-            invalidatesTags: ['Product'],
+            invalidatesTags: ['Products'],
         }),
     }),
 });

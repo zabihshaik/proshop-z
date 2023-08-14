76b8b828883ccf529f52cdc4855c3a4f03edcf02
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-14 21:23:47 +0530

    Multer & Image Upload Endpoint-BE

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/'); // null is for error
+    },
+    filename(req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+function checkFileType(file, cb) {
+    const fileTypes = /jpg|jpeg|png/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb('Images Only!');
+    }
+} 
+
+const upload = multer({
+    storage,
+});
+
+router.post('/', upload.single('image'), (req,res) => {
+    res.send({
+        message: 'Image Uploaded',
+        image: `/${req.file.path}`
+    });
+});
+
+export default router;

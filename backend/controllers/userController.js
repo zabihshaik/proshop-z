b50035809c794bd9e19e108ee48838d9da0f1013
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-15 23:14:32 +0530

    Backend user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -128,7 +128,8 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 
 const getUsers = asyncHandler(async (req, res) => {
-    res.send('get users');
+    const users = await User.find({});
+    res.status(200).json(users);
 });
 
 // @desc    Get User by ID
@@ -136,7 +137,14 @@ const getUsers = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 
 const getUserbyID = asyncHandler(async (req, res) => {
-    res.send('get user by ID');
+    const user = await User.findById(req.params.id).select('-password');
+
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc    Delete User
@@ -144,7 +152,19 @@ const getUserbyID = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 
 const deleteUser = asyncHandler(async (req, res) => {
-    res.send('delete user');
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        if(user.isAdmin) {
+            res.status(400);
+            throw new Error('Cannot delete admin user');
+        }
+        await User.deleteOne({ _id: user._id });
+        res.status(200).json({ message: 'User deleted successfully' });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc    Update User
@@ -152,7 +172,25 @@ const deleteUser = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 
 const updateUser = asyncHandler(async (req, res) => {
-    res.send('update user');
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
  export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserbyID, deleteUser, updateUser};

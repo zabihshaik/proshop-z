3bdfae3b8d4a9bd05dacc2524f090ced85a11707
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-16 20:51:41 +0530

    Delete Users by Admin

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -3,18 +3,30 @@ import { Table, Button } from 'react-bootstrap';
 import { FaTimes, FaTrash, FaEdit, FaCheck } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-import { useGetUsersQuery } from '../../slices/usersApiSlice';
+import { toast } from 'react-toastify';
+import { useGetUsersQuery, useDeleteUserMutation } from '../../slices/usersApiSlice';
 
 
 const UserListScreen = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
-  const deleteHandler = () => {
-    console.log('delete');
+  const [deleteUser, { isLoading: loadingDelete }] = useDeleteUserMutation();
+
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure to delete User?')) {
+        try {
+            await deleteUser(id);
+            toast.success('User deleted');
+            refetch();
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
+    }
   };
   
   return <>
     <h1>Users</h1>
+    {loadingDelete && <Loader />}
     {isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
       <Table striped hover responsive className='table-sm'>
         <thead>
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -37,7 +37,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             providesTags: ['Users'],
             keepUnusedDataFor: 5,
         }),
+        deleteUser: builder.mutation({
+            query: (userId) => ({
+                url: `${USERS_URL}/${userId}`,
+                method: 'DELETE',
+            }),
+        }),
     }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation } = usersApiSlice;

9cb04e48baf1d4858ce7b21adfdb2ec3d4c00d94
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-24 23:55:43 +0530

    Fix PayPal & Price Calculate Bugs

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -46,7 +46,7 @@ const OrderScreen = () => {
     function onApprove(data, actions) {
         return actions.order.capture().then(async function (details) {
             try {
-                await payOrder({ orderId, details });
+                await payOrder({ orderId, details }).unwrap();
                 refetch();
                 toast.success('Payment successful');
             } catch (err) {

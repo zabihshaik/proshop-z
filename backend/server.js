0454b8b5a0a1f88781ea4862352f46c6c2ddbecb
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-25 22:45:49 +0530

    Prepare for Production

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 connectDB(); // Connect to MongoDB
 
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running....');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -36,6 +32,20 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 
 const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    // any route that is not api, will be redirected to index.html
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);

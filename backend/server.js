6d66b2fb6c26be2631831d9ceb579049c77a45e2
Zabihulla Shaik <informshaikz@gmail.com>
2023-08-26 23:14:08 +0530

    Fix small bugs

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000 ;
 
 connectDB(); // Connect to MongoDB
 
